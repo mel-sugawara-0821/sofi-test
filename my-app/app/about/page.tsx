1aee936bdc871dd574772bd3b7389a552f606dc9
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-01-26 07:58:50 +0900

    app-router

---
+++ my-app/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <main>
+            <h1 className="font-bold mb-4">About</h1>
+            <p className="text-gray-600">This is the about page.</p>
+        </main>
+    )
+}
