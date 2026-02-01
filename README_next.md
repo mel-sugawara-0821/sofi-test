1b8a979936be2b87968c90bd8c4ec0fcb2132082
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-02-01 15:42:16 +0900

    topページ作成

--- README_next.md
+++ README_next.md
@@ -3,3 +3,10 @@ Q. SSRとは？
 App Router
     ・my-app/app/about/page.tsx を作る
     ・/about でページができている。。！
+
+sec9. ヘッダー
+・Linkコンポーネント
+
+TODO
+・画面プレビュー
+・
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,44 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import Image from 'next/image'
+import beast_img from "../public/beast.jpg";
+
 export default function Home() {
+  const [currentDate, setCurrentDate] = useState<string>('');
+
+  useEffect(() => {
+    updateDate();
+    const interval = setInterval(updateDate, 1000);
+    return () => clearInterval(interval);
+  }, [])
+
+  const updateDate = () => {
+    const newDate = new Date();
+    const formatedDate = newDate.toLocaleString("ja-JP", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      weekday: "long",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit"
+    })
+    setCurrentDate(formatedDate);
+  }
+
   return (
-    <main className="container mx-auto px-4 py-8">
+    <main className="container home text-center mx-auto px-4 py-8">
       <div>
         <h1>秘密のブログ</h1>
         <p>Next.js学習者のためのサイト</p>
+        <div>{currentDate}</div>
+        <div className="mx-auto">
+          <Image
+            src={beast_img}
+            style={{margin: 'auto'}}
+           />
+        </div>
       </div>
     </main>
   );
