86132ad916de9ea511da0b830060f26a568a5be8
mel-sugawara-0821 <sugawara@meldigital.jp>
2026-02-02 07:32:53 +0900

    Feature/static page 8 (#7)
    * 静的ページ作成
    
    * topページ作成
    
    * alt属性追加
    
    * deploy
    
    * 修正
    
    * 修正
    
    * debug
    
    * 修正
    
    ---------
    
    Co-authored-by: mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>

---
+++ my-app/app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+    return (
+        <main className="container mx-auto px-4 py-8">
+            <div>
+            <h1>お問い合わせ</h1>
+            <p>Next.js学習者のためのサイト</p>
+          </div>
+        </main>
+    )
+}
