0d61c290d1c74ddb61bf787a7ae47b9157e6874c
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-02-14 11:26:13 +0900

    CSSモジュール導入

--- my-app/app/about/page.tsx
+++ my-app/app/about/page.tsx
@@ -1,7 +1,9 @@
+import styles from './about.module.scss'
+
 export default async function About() {
     await new Promise(resolve => setTimeout(resolve, 1000));
     return (
-        <main>
+        <main className={styles.about}>
             <h1 className="font-bold mb-4">About</h1>
             <p className="text-gray-600">This is the about page.</p>
         </main>
