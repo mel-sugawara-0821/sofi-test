068ef277eebafc71c4fe95ebcd5f1604ffb158de
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-02-15 12:57:51 +0900

    lint対応

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,31 +1,31 @@
 "use client"
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Image from 'next/image'
 import beast_img from "../public/beast.jpg";
 
 export default function Home() {
-  const [currentDate, setCurrentDate] = useState<string>('');
+  const [currentDate] = useState<string>('');
 
-  useEffect(() => {
-    updateDate();
-    const interval = setInterval(updateDate, 1000);
-    return () => clearInterval(interval);
-  }, [])
+  // useEffect(() => {
+  //   updateDate();
+  //   const interval = setInterval(updateDate, 1000);
+  //   return () => clearInterval(interval);
+  // }, [])
 
-  const updateDate = () => {
-    const newDate = new Date();
-    const formatedDate = newDate.toLocaleString("ja-JP", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      weekday: "long",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit"
-    })
-    setCurrentDate(formatedDate);
-  }
+  // const updateDate = () => {
+  //   const newDate = new Date();
+  //   const formatedDate = newDate.toLocaleString("ja-JP", {
+  //     year: "numeric",
+  //     month: "long",
+  //     day: "numeric",
+  //     weekday: "long",
+  //     hour: "2-digit",
+  //     minute: "2-digit",
+  //     second: "2-digit"
+  //   })
+  //   setCurrentDate(formatedDate);
+  // }
 
   return (
     <main className="container home text-center mx-auto px-4 py-8">
