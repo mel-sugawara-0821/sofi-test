dfa7231336782274ced2407b0af587a3faa5842e
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-02-18 16:46:22 +0900

    スマホメニュー追加

--- my-app/components/Navigation.tsx
+++ my-app/components/Navigation.tsx
@@ -18,20 +18,60 @@ export default function Navigation() {
         { href: '/contact', label: 'Contact'},
     ]
 
-    const isActive = (href: string):boolean => {
+    const isActive = (href: string): boolean | null => {
+        if (href === '/') {
+            return path_name === href;
+        }
         console.log('href-----')
         console.log(href)
-        return true
-
+        return path_name.startsWith(href)
     }
 
     return (
         <header className='flex justify-around bg-gray-100 shadow-sm text-gray-600'>
-          <nav>
-            <div className='flex justify-around w-100'>
-                <div>head</div>
-            </div>
-          </nav>
+            <nav>
+                <div className='flex justify-around w-100'>
+                    {navItems.map((navItem) => (
+                        <Link
+                            key={navItem.href}
+                            href={navItem.href}
+                            className={`${isActive(navItem.href) ? "text-blue-600" : "text-gray-600"}`}
+                            >{navItem.label}
+                        </Link>
+                    ))}
+                </div>
+                {/* spのナビ */}
+                <button 
+                    onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+                    className="">
+                    <svg
+                        className="h-6 w-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </button>
+                {/* モバイルメニュー */}
+                {isMobileMenuOpen && (
+                    <div className="md:hidden">
+                        <div>
+                            {navItems.map((navItem) => (
+                                <Link
+                                    key={navItem.href}
+                                    href={navItem.href}
+                                    className={`${isActive(navItem.href) ? "text-blue-600" : "text-gray-600"}`}
+                                    >{navItem.label}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </nav>
         </header>
     )
 }
