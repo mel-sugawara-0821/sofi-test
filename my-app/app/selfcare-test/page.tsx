a25aa744b5c09ff1f43cf3bb6bdfe8c7326881ef
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-02-21 11:11:32 +0900

    スライダー実装

--- my-app/app/selfcare-test/page.tsx
+++ my-app/app/selfcare-test/page.tsx
@@ -1,12 +1,11 @@
-import Link from "next/link";
 import { getBlogs } from '@/libs/client';
-
 import styles from './selfcares.module.scss'
+import Test from '@/components/Test';
 
 export default async function SelfCareIndex() {
     const { contents }  = await getBlogs();
 
-    // TODO: エラーハンドリング
+    // // TODO: エラーハンドリング
     if (!contents) {
       return <h1>No Contents</h1>;
     }
@@ -15,14 +14,7 @@ export default async function SelfCareIndex() {
     return (
         <div className={styles.selfcare}>
             <h2>セルフケアtest一覧</h2>
-            <ul>
-                {contents.map((blog) => (
-                    <li key={blog.id}>
-                        {/* TODO: urlも動的生成したい */}
-                        <Link href={`/selfcare-test/${blog.id}`}>{blog.title}</Link>
-                    </li>
-                ))}
-            </ul>
+            <Test contents={contents}/>
         </div>
     )
 }
---
+++ my-app/components/Test.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import Link from "next/link";
+import styles from './selfcares.module.scss'
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import '@splidejs/splide/css'; // デフォルトのテーマを読み込んでいます（コアスタイルのみ読み込む設定も可能）
+
+export default  function Test(props: any) {
+    return (
+        <div className={styles.selfcare}>
+            <Splide
+                options={{
+                    mediaQuery: 'min',
+                    perMove: 1, //1度に何枚動かすか
+                    fixedWidth: '240px',
+                    gap: 16,
+                    updateOnMove: true,
+                    breakpoints: {
+                      1025: {
+                        fixedWidth: '360px',
+                        gap: 32,
+                      }
+                    },
+                }}            
+            >
+                {props.contents.map((blog) => (
+                    <SplideSlide key={blog.id}>
+                        {/* TODO: urlも動的生成したい */}
+                        <Link href={`/selfcare-test/${blog.id}`}>{blog.title}</Link>
+                    </SplideSlide>
+                ))}
+            </Splide>
+        </div>
+    )
+}
